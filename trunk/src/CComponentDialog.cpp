879a29396e493eb1e020cc108f88a231f5143817
hongduc.pr <hongduc.pr@gmail.com>
2012-03-12 10:47:03 +0000

    - Add dialog

---
+++ trunk/src/CComponentDialog.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "CComponentDialog.h"
+
+CComponentDialog::CComponentDialog(LPWSTR lpTitle, int x, int y, int w, int h, uiWindow* pParent)
+	:CBaseDialog(lpTitle, x, y, w, h, pParent)
+{
+	
+}
+
+CComponentDialog::~CComponentDialog()
+{
+}

0d3941d574e1847f65351296d4e9eac84ad44a51
hongduc.pr <hongduc.pr@gmail.com>
2012-03-19 08:50:28 +0000

    - test

--- trunk/prj/snake3d/snake3d.cpp
+++ trunk/prj/snake3d/snake3d.cpp
@@ -18,6 +18,10 @@ class CApplication: public IEventReceiver, IView
 public:
 	long	m_lastUpdateTime;
 	int		m_fps;
+
+protected:
+	ITexture*		m_image;
+	gui::IGUIFont*	m_font;
 public:
 
 	virtual bool OnEvent(const SEvent& event)
@@ -40,6 +44,17 @@ public:
 
 		m_lastUpdateTime =  m_device->getTimer()->getTime();
 		m_fps = 0;
+
+		m_device->setWindowCaption( L"Snake3D" );
+
+		// load image
+		m_image = m_driver->getTexture("data/irrlichtlogo.png");
+		m_font	= device->getGUIEnvironment()->getFont("data/bigfont.png");
+	}
+
+	void destroyApplication()
+	{
+		
 	}
 
 	void mainLoop()
@@ -49,21 +64,31 @@ public:
 		m_lastUpdateTime = now;
 		
 		m_driver->beginScene(true, true, video::SColor(255,113,113,133));
+		
 		m_smgr->drawAll();
-		m_driver->endScene();
 
+		// draw image
+		m_driver->draw2DImage
+			(
+				m_image, 
+				core::position2d<s32>(10,40),
+				core::rect<s32>(0,0, m_image->getSize().Width, m_image->getSize().Height), 0,
+				video::SColor(255,255,255,255), 
+				true
+			);
+		
 		int fps = m_driver->getFPS();
 
-		if (m_fps != fps)
-		{
-			core::stringw tmp(L"Snake 3D - Irrlicht Engine [");
-			tmp += m_driver->getName();
-			tmp += L"] fps: ";
-			tmp += fps;
-
-			m_device->setWindowCaption(tmp.c_str());
-			m_fps = fps;
-		}
+		// draw fps string		
+		core::stringw tmp(L"Snake 3D - ");
+		tmp += m_driver->getName();
+		tmp += L" fps: ";
+		tmp += fps;
+			
+		m_font->draw( tmp.c_str(), core::rect<s32>(10,10,300,50), video::SColor(255,255,255,255));			
+		m_fps = fps;
+		
+		m_driver->endScene();
 	}
 
 };
@@ -84,6 +109,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 		myApp.mainLoop();		
 	}
 
+	myApp.destroyApplication();
 	device->drop();
 
 	UIDEBUG_TRACE("Debug leak memory: \n");

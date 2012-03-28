c4e492c1338824c95cf8d6da89f8ae262a1daa48
hongduc.pr <hongduc.pr@gmail.com>
2012-03-28 10:41:57 +0000

    - add function for trigger

--- trunk/src/core/GameObject/CTrigger.h
+++ trunk/src/core/GameObject/CTrigger.h
@@ -8,6 +8,15 @@ class CTrigger: public CGameObject
 protected:
 	float m_boxSize;
 
+	std::string m_scriptFile;
+	std::string m_onAlways;
+	std::string	m_onWhileIn;
+	std::string	m_onWhileOut;
+	std::string m_onEnter;
+	std::string	m_onExit;
+	std::string m_onEnable;
+	std::string m_onDisable;
+
 public:
 	CTrigger();
 	virtual ~CTrigger();

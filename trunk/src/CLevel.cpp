01cfc183315952803497f65d17d9452fa7df12d4
hongduc.pr <hongduc.pr@gmail.com>
2012-03-31 03:48:01 +0000

    - Test terrain map

--- trunk/src/CLevel.cpp
+++ trunk/src/CLevel.cpp
@@ -32,6 +32,11 @@ void CLevel::init()
 	m_playerCamera = new CGameCamera();
 	m_playerCamera->setPosition( core::vector3df(-100, 100, -100) );
 	m_playerCamera->setTarget( core::vector3df( 0, 0, 0) );
+
+	CGameGSCameraAnimators* animator = new CGameGSCameraAnimators( getIView()->getDevice()->getCursorControl() );
+	m_playerCamera->getCameraNode()->addAnimator( animator );
+	animator->drop();
+
 	getIView()->setActiveCamera( m_playerCamera );
 
 	// spawn orge character	
@@ -48,6 +53,28 @@ void CLevel::init()
 		skyBox->setVisible( true );
 
 	ILightSceneNode *light = smgr->addLightSceneNode( smgr->getRootSceneNode() );
+
+
+	// add terrain
+	scene::ITerrainSceneNode* terrain = smgr->addTerrainSceneNode(
+		"data/terrain-heightmap.bmp",
+		0,										// parent node
+		-1,										// node id
+		core::vector3df(0.f, 0.f, 0.f),			// position
+		core::vector3df(0.f, 0.f, 0.f),			// rotation
+		core::vector3df(40.f, 4.4f, 40.f),		// scale
+		video::SColor ( 255, 255, 255, 255 ),	// vertexColor
+		5,										// maxLOD
+		scene::ETPS_17,							// patchSize
+		4										// smoothFactor
+		);
+
+	terrain->setMaterialFlag(video::EMF_LIGHTING, false);
+	terrain->setMaterialTexture(0,getIView()->getDriver()->getTexture("data/terrain-texture.jpg"));
+	terrain->setMaterialTexture(1,getIView()->getDriver()->getTexture("data/detailmap3.jpg"));	
+	terrain->setMaterialType(video::EMT_DETAIL_MAP);
+
+	terrain->scaleTexture(1.0f, 20.0f);
 }
 
 void CLevel::update()

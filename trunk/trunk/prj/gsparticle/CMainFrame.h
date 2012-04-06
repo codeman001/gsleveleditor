98a4b0fd8723414cd9d9502ad799a52bd70e0ed7
hongduc.pr@gmail.com <hongduc.pr@gmail.com@46c6b5e2-aea9-36ba-7a94-b5413ff3bfb8>
2012-04-06 03:55:07 +0000

    - update toolbar

--- trunk/trunk/prj/gsparticle/CMainFrame.h
+++ trunk/trunk/prj/gsparticle/CMainFrame.h
@@ -14,6 +14,7 @@ protected:
 
 	uiToolbarButton*	m_addButton;
 	uiToolbarButton*	m_delButton;
+	uiToolbarButton*	m_playStopParticleButton;
 public:
 	CMainFrame();
 	virtual ~CMainFrame();
@@ -51,6 +52,7 @@ protected:
 	
 	void onToolbarAdd( uiObject *pSender );
 	void onToolbarDel( uiObject *pSender );
+	void onToolbarPlayStopParticle( uiObject *pSender );
 };
 
 #endif

36bc829c1958229601edbfe250ec51450209f0f0
hongduc.pr@gmail.com <hongduc.pr@gmail.com@46c6b5e2-aea9-36ba-7a94-b5413ff3bfb8>
2012-04-08 17:38:23 +0000

    - update save, load xml on Particle

--- trunk/trunk/src/core/Component/CParticleComponent.cpp
+++ trunk/trunk/src/core/Component/CParticleComponent.cpp
@@ -28,6 +28,7 @@ void CParticleComponent::updateComponent()
 void CParticleComponent::saveData( CSerializable* pObj )
 {
 	pObj->addGroup( IObjectComponent::s_compType[ m_componentID ] );
+	pObj->addPath( "particleXml", m_xmlPath.c_str() );
 }
 
 // loadData
@@ -35,7 +36,7 @@ void CParticleComponent::saveData( CSerializable* pObj )
 void CParticleComponent::loadData( CSerializable* pObj )
 {
 	pObj->nextRecord();
-	
+	m_xmlPath = pObj->readString();
 	initParticle();
 }
 
@@ -49,6 +50,9 @@ void CParticleComponent::initParticle()
 	// create an empty node
 	m_gameObject->m_node = smgr->addEmptySceneNode( smgr->getRootSceneNode() );
 	m_gameObject->m_node->grab();
+
+	// load particle
+	loadXML( m_xmlPath.c_str() );
 }
 
 // createParticle
@@ -77,6 +81,21 @@ void CParticleComponent::removeParticle( int i )
 		ps->remove();
 		m_arrayParticle.erase( m_arrayParticle.begin() + i );
 	}
+}
+
+// removeAllParticle
+// remove all particle
+void CParticleComponent::removeAllParticle()
+{
+	vector<SParticleInfo>::iterator i = m_arrayParticle.begin(), end = m_arrayParticle.end();
+
+	while ( i != end )
+	{
+		(*i).ps->remove();
+		i++;
+	}
+	
+	m_arrayParticle.clear();
 }
 
 // getParticleInfo
@@ -113,4 +132,149 @@ int CParticleComponent::getParticleID( IParticleSystemSceneNode *ps )
 		i++;
 	}
 	return -1;
+}
+
+// saveXML
+// save list particle to xml file
+void CParticleComponent::saveXML( const char *lpFileName )
+{
+	IrrlichtDevice *device = getIView()->getDevice();
+	io::IFileSystem *fs = device->getFileSystem();
+
+	io::IWriteFile *file = fs->createAndWriteFile( lpFileName );
+	if ( file == NULL )
+		return;
+
+	const unsigned short UTF16_LE = 0xFEFF;
+	file->write( (void*)&UTF16_LE, sizeof(unsigned short) );
+
+	io::IXMLWriter *xmlWrite = fs->createXMLWriter( file );
+	if ( xmlWrite == NULL )
+		return;	
+	
+	xmlWrite->writeElement(L"particles");
+	xmlWrite->writeLineBreak();
+
+	io::IAttributes *attrb = fs->createEmptyAttributes();
+	
+	wchar_t lpTemp1[1024];
+	wchar_t lpTemp2[1024];
+
+	vector<SParticleInfo>::iterator i = m_arrayParticle.begin(), end = m_arrayParticle.end();
+	while (i != end )
+	{
+		SParticleInfo& p = (*i);	
+
+					
+		p.ps->serializeAttributes( attrb );
+
+		uiString::copy<wchar_t,const irr::c8>(lpTemp1, p.ps->getName());
+		uiString::copy<wchar_t,const char>(lpTemp2, p.texture.c_str());
+
+		xmlWrite->writeElement(L"ps",false, L"name", lpTemp1, L"texture", lpTemp2);
+		xmlWrite->writeLineBreak();
+
+		// write <attribute>
+		attrb->write( xmlWrite );
+
+		xmlWrite->writeClosingTag(L"ps");
+		xmlWrite->writeLineBreak();
+
+		attrb->clear();
+
+		i++;
+	}
+
+	xmlWrite->writeClosingTag(L"particles");
+	
+	xmlWrite->drop();
+	file->drop();
+}
+
+// loadXML
+// load particle from xml file
+void CParticleComponent::loadXML( const char *lpFileName )
+{
+	removeAllParticle();
+	
+	IrrlichtDevice *device = getIView()->getDevice();
+	IVideoDriver *driver = getIView()->getDriver();
+	io::IFileSystem *fs = device->getFileSystem();
+
+	io::IXMLReader *xmlRead = fs->createXMLReader( lpFileName );
+	if ( xmlRead == NULL )
+		return;	
+
+	IParticleSystemSceneNode *particle = NULL;
+
+	while ( xmlRead->read() )
+	{
+		if (core::stringw(L"particles") == xmlRead->getNodeName())
+		{
+			while( xmlRead->read() )
+			{
+				switch (xmlRead->getNodeType())
+				{
+				case io::EXN_ELEMENT_END:
+					break;
+				case io::EXN_ELEMENT:
+					{
+						if (core::stringw(L"ps") == xmlRead->getNodeName())
+						{
+							particle = createParticle();
+					
+							const wchar_t *attribValue = xmlRead->getAttributeValue(L"name");
+							char attribValueA[1024] = {0};
+							uiString::convertUnicodeToUTF8( (unsigned short*)attribValue, attribValueA );
+							particle->setName( attribValue );
+
+							attribValue = xmlRead->getAttributeValue(L"texture");							
+							uiString::convertUnicodeToUTF8( (unsigned short*)attribValue, attribValueA );							
+
+							ITexture *pTex = driver->getTexture( attribValueA );
+
+#if defined(GSEDITOR) || defined(PARTICLE_EDITOR)
+							if ( pTex == NULL )
+							{
+								WCHAR appPath[MAX_PATH];
+								char  appPathA[MAX_PATH];
+
+								uiApplication::getAppPath(appPath, MAX_PATH);
+								uiString::copy<char, WCHAR>( appPathA, appPath  );
+								
+								std::string path = appPathA;
+								path += "\\";
+								path += std::string(attribValueA);
+								
+								pTex = driver->getTexture(path.c_str());
+							}
+#endif
+							if ( pTex && particle )
+							{
+								particle->setMaterialTexture(0, pTex );
+								particle->setMaterialFlag(	video::EMF_LIGHTING, false );
+								particle->setMaterialType(	video::EMT_TRANSPARENT_ADD_COLOR);
+							}
+						}
+						else if (core::stringw(L"attributes") == xmlRead->getNodeName())
+						{
+							io::IAttributes* attributes = fs->createEmptyAttributes();
+							attributes->read(xmlRead, true);
+
+							if ( particle )
+								particle->deserializeAttributes( attributes );
+							
+							attributes->drop();
+							particle = NULL;
+						}
+					}
+					break;
+				default:
+					break;
+				}
+			}
+		}
+	}
+
+	xmlRead->drop();
 }

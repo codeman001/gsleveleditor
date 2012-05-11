4be88222eee0b4d04ddc88d3159fd673c3b3f890
hongduc.pr@gmail.com <hongduc.pr@gmail.com@46c6b5e2-aea9-36ba-7a94-b5413ff3bfb8>
2012-05-11 06:57:09 +0000

    - add water shader

--- trunk/trunk/src/core/SceneNode/CGameWaterSceneNode.cpp
+++ trunk/trunk/src/core/SceneNode/CGameWaterSceneNode.cpp
@@ -1,15 +1,83 @@
 #include "stdafx.h"
 #include "CGameWaterSceneNode.h"
+#include "IView.h"
 
 CGameWaterSceneNode::CGameWaterSceneNode(CGameObject *owner, IMesh* mesh, ISceneNode* parent, ISceneManager* mgr, 
 		const core::vector3df& position,
 		s32 id)
 	:ISceneNode( parent, mgr, id, position, core::vector3df(0,0,0) )
 {
+	m_mesh = mesh;
+	m_mesh->grab();
+
+	static s32 s_materialWater = -1;
+	if ( s_materialWater == -1 )
+	{		
+		CWaterGrassCallBack* waterShaderCB = new CWaterGrassCallBack();
+
+		s_materialWater = mgr->getVideoDriver()->getGPUProgrammingServices()->addHighLevelShaderMaterialFromFiles
+		(
+			"shader/extern/water.vert", "vertexMain",	video::EVST_VS_2_0,
+			"shader/extern/water.frag", "pixelMain",	video::EPST_PS_2_0,
+			waterShaderCB, 
+			video::EMT_TRANSPARENT_ALPHA_CHANNEL
+		);
+
+		waterShaderCB->drop();		
+	}
+	
+	setMaterialType( (E_MATERIAL_TYPE)s_materialWater );
 
 }
 
 CGameWaterSceneNode::~CGameWaterSceneNode()
 {
+	m_mesh->drop();
+}
 
+// OnRegisterSceneNode
+// ISceneNode implement
+void CGameWaterSceneNode::OnRegisterSceneNode()
+{
+	if (IsVisible)
+        SceneManager->registerNodeForRendering(this);
+
+    ISceneNode::OnRegisterSceneNode();
+}
+
+// render
+// ISceneNode implement
+void CGameWaterSceneNode::render()
+{
+	ISceneManager *smgr = getIView()->getSceneMgr();
+	IVideoDriver *driver = smgr->getVideoDriver();
+
+	driver->setTransform( video::ETS_WORLD, AbsoluteTransformation );    
+
+    for(int i=0; i < (int)m_mesh->getMeshBufferCount();i++)
+    {
+        driver->setMaterial( m_mesh->getMeshBuffer(i)->getMaterial() );
+        driver->drawMeshBuffer(m_mesh->getMeshBuffer(i));
+    }
+}
+
+// getBoundingBox
+// ISceneNode implement
+const core::aabbox3d<f32>& CGameWaterSceneNode::getBoundingBox() const
+{
+	return m_mesh->getBoundingBox();
+}
+
+// getMaterialCount
+// ISceneNode implement
+u32 CGameWaterSceneNode::getMaterialCount() const
+{
+	return 1;
+}
+
+// getMaterial
+// ISceneNode implement
+video::SMaterial& CGameWaterSceneNode::getMaterial(u32 i)
+{
+	return m_mesh->getMeshBuffer(i)->getMaterial();
 }
--- trunk/trunk/src/core/SceneNode/CGameWaterSceneNode.h
+++ trunk/trunk/src/core/SceneNode/CGameWaterSceneNode.h
@@ -3,15 +3,65 @@
 
 #include "stdafx.h"
 #include "CGameObject.h"
+#include "IView.h"
+
+class CWaterGrassCallBack : public video::IShaderConstantSetCallBack
+{
+private:
+	int normalMap;
+	int reflectionmap;
+
+public:
+    CWaterGrassCallBack()
+    {		
+		normalMap = 0;
+		reflectionmap = 1;
+    }
+
+    virtual void OnSetConstants(video::IMaterialRendererServices* services, s32 userData)
+    {
+		IrrlichtDevice *device = getIView()->getDevice();
+
+		float time = device->getTimer()->getTime()/1000.0f;
+
+		services->setPixelShaderConstant("waterTime",	(float*)&time, 1);
+		services->setPixelShaderConstant("NormalMap",	(float*)&normalMap, 1);
+		services->setPixelShaderConstant("t1",			(float*)&reflectionmap, 1);
+    }
+};
+
 
 class CGameWaterSceneNode: public ISceneNode
 {
+protected:
+	IMesh* m_mesh;
 public:
 	CGameWaterSceneNode(CGameObject *owner, IMesh* mesh, ISceneNode* parent, ISceneManager* mgr, 
 		const core::vector3df& position = core::vector3df(0,0,0), 
 		s32 id = -1);
 
     virtual ~CGameWaterSceneNode();
+
+	// OnRegisterSceneNode
+	// ISceneNode implement
+	virtual void OnRegisterSceneNode();
+
+	// render
+	// ISceneNode implement
+	virtual void render();
+
+	// getBoundingBox
+	// ISceneNode implement
+	virtual const core::aabbox3d<f32>& getBoundingBox() const;
+
+	// getMaterialCount
+	// ISceneNode implement
+	virtual u32 getMaterialCount() const;
+
+	// getMaterial
+	// ISceneNode implement
+	virtual video::SMaterial& getMaterial(u32 i);
+
 };
 
 #endif

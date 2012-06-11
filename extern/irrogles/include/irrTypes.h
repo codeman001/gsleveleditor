9281f6eb541dd03adb00880a4c545161d10e1534
hongduc.pr <hongduc.pr@gmail.com>
2012-06-11 14:08:49 +0000

    - revert to old gameswf version

--- extern/irrogles/include/irrTypes.h
+++ extern/irrogles/include/irrTypes.h
@@ -95,18 +95,11 @@ typedef double				f64;
 //! These should be int snprintf(char *str, size_t size, const char *format, ...);
 //! and int swprintf(wchar_t *wcs, size_t maxlen, const wchar_t *format, ...);
 #if defined(_MSC_VER) && _MSC_VER > 1310 && !defined (_WIN32_WCE)
-
-#ifndef swprintf 
-	#define swprintf swprintf_s
-#endif
-
-#ifndef snprintf
-	#define snprintf sprintf_s
-#endif
-
+#define swprintf swprintf_s
+#define snprintf sprintf_s
 #else
-	#define swprintf _snwprintf
-	#define snprintf _snprintf
+#define swprintf _snwprintf
+#define snprintf _snprintf
 #endif
 
 // define the wchar_t type if not already built in.

5853161cefd3cea5523ddca390dae4474693bf64
hongduc.pr@gmail.com <hongduc.pr@gmail.com@46c6b5e2-aea9-36ba-7a94-b5413ff3bfb8>
2012-07-03 05:06:26 +0000

    - implement EllipsoidCollision

--- trunk/trunk/src/core/Component/CEllipsoidCollisionComponent.cpp
+++ trunk/trunk/src/core/Component/CEllipsoidCollisionComponent.cpp
@@ -10,7 +10,13 @@
 CEllipsoidCollisionComponent::CEllipsoidCollisionComponent( CGameObject *pObj )
 	:IObjectComponent( pObj, IObjectComponent::EllipsoidCollision )
 {
-	m_isFirstUpdate = true;
+	m_isFirstUpdate		= true;
+	m_isFalling			= true;
+	
+	m_gravity			= core::vector3df(0,-10,0);
+	
+	m_ellipsoidRadius	= core::vector3df(30,60,30);
+	m_translation		= core::vector3df(0,-60,0);
 }
 
 CEllipsoidCollisionComponent::~CEllipsoidCollisionComponent()
@@ -50,6 +56,14 @@ void CEllipsoidCollisionComponent::updateComponent()
 		return;
 	}	
 
+	// get position need goto
+	m_testPosition = m_gameObject->getPosition();
+		
+	// calc gravity	
+	float diff = getIView()->getTimeStep();
+	
+	m_fallingVelocity += m_gravity * (f32)diff * 0.001f;
+
 	// my bouding box
 	core::aabbox3df myBox = node->getTransformedBoundingBox();
 
@@ -63,7 +77,6 @@ void CEllipsoidCollisionComponent::updateComponent()
 		
 		if ( zoneBox.intersectsWithBox( myBox ) == true )
 		{
-
 			// loop all terrain object in zone
 			ArrayGameObject& terrains = z->getTerrainList();
 			ArrayGameObject::iterator itTerrain = terrains.begin(), itTerrainEnd = terrains.end();
@@ -86,12 +99,18 @@ void CEllipsoidCollisionComponent::updateComponent()
 						}
 					}
 				}
-
+				
 				itTerrain++;
 			}
 		}
 
 	}
+
+	// set new position
+	m_gameObject->setPosition( m_testPosition );
+
+	// save last position
+	m_lastPosition = m_testPosition;
 #endif
 }
 
@@ -100,7 +119,44 @@ void CEllipsoidCollisionComponent::updateComponent()
 void CEllipsoidCollisionComponent::collideTest( ITriangleSelector *world )
 {
 #ifdef GSGAMEPLAY
+	core::vector3df vel = m_lastPosition - m_testPosition;
+
+	core::triangle3df	collisionTriangle;
+	core::vector3df		collisionPoint;
+	core::vector3df		collisionResultPosition;
+	ISceneNode			*collisionNode = 0;	
 	
+	bool f = false;
+	f32 slidingSpeed = 0.0005f;
+
+	// detect collision
+	collisionResultPosition  = getIView()->getSceneMgr()->getSceneCollisionManager()->getCollisionResultPosition
+		(
+			world, m_lastPosition - m_translation,
+			m_ellipsoidRadius, 
+			vel, 
+			collisionTriangle,	// out triangle
+			collisionPoint,		// out point
+			f,
+			collisionNode,		// out node
+			slidingSpeed,
+			m_fallingVelocity
+		);
+		
+	collisionResultPosition += m_translation;
+
+	if (f == false)
+	{
+		// has ground position
+		m_isFalling = false;
+		m_fallingVelocity.set(0, 0, 0);
+	}
+	else
+	{
+		m_isFalling = true;
+	}
+
+	m_testPosition = collisionResultPosition;
 #endif
 }
 

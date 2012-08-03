12d2f846af1fc196a63bb67578dd6e5fa82a24b6
hongduc.pr <hongduc.pr@gmail.com>
2012-08-03 11:55:54 +0000

    - update animation blending window (trackbar)

--- trunk/prj/gsanimation/CAnimBlendingFrame.cpp
+++ trunk/prj/gsanimation/CAnimBlendingFrame.cpp
@@ -4,12 +4,22 @@
 CAnimBlendingFrame::CAnimBlendingFrame( LPWSTR lpTitle, int x, int y, int w, int h, uiWindow* pParent )
 	:uiWindow( lpTitle, x,y,w,h, pParent )
 {
+	m_colladaComponent = NULL;
+
+	m_trackbar = ref<uiTrackBar>( new uiTrackBar( L"Trackbar", 0,0, 200, 40, this, true, true ) );
+	m_trackbar->setRange( 0,10 );
 }
 
 CAnimBlendingFrame::~CAnimBlendingFrame()
 {
 }
-	
+
+// _OnHScroll
+// event when scroll
+void CAnimBlendingFrame::_OnVScroll( uiScrollBarEvent scrollEvent )
+{
+}
+
 // messageMap
 // Phan tich su kien cho uiForm
 LRESULT	CAnimBlendingFrame::messageMap(HWND hWnd,UINT uMsg, WPARAM wParam, LPARAM lParam)
--- trunk/prj/gsanimation/CAnimBlendingFrame.h
+++ trunk/prj/gsanimation/CAnimBlendingFrame.h
@@ -8,18 +8,22 @@ class CAnimBlendingFrame: public uiWindow
 {
 protected:
 	CColladaMeshComponent	*m_colladaComponent;
-
+	uiTrackBar				*m_trackbar;
 public:
 	CAnimBlendingFrame( LPWSTR lpTitle, int x, int y, int w, int h, uiWindow* pParent );
 	virtual ~CAnimBlendingFrame();
 	
+	// _OnVScroll
+	// event when scroll
+	virtual void _OnVScroll( uiScrollBarEvent scrollEvent );
+
 	// messageMap
 	// Phan tich su kien cho uiForm
 	virtual LRESULT	messageMap(HWND hWnd,UINT uMsg, WPARAM wParam, LPARAM lParam);
 	
 	// setColladaComponent
 	// update new collada scene
-	void setColladaComponent( CColladaMeshComponent *comp );
+	void setColladaComponent( CColladaMeshComponent *comp );	
 };
 
 #endif

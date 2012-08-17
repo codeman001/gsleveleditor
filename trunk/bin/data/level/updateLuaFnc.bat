93d45f90029f6fa97936e93d7dd631415f68c07d
hongduc.pr <hongduc.pr@gmail.com>
2012-08-17 05:23:29 +0000

    - add lua function sort tool

--- trunk/bin/data/level/updateLuaFnc.bat
+++ trunk/bin/data/level/updateLuaFnc.bat
@@ -1,2 +1,4 @@
 set NPPPATH=C:\Program Files\Notepad++\plugins\APIs
+set LUACODEDEF="..\..\..\tools\luaCodeDefine\Release\luaCodeDefine.exe"
+%LUACODEDEF% luaFunction.xml lua.xml
 copy lua.xml "%NPPPATH%\lua.xml"

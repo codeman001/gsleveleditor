9390e946c5afe4b28cc35b95168d8493d2016e38
hongduc.pr@gmail.com <hongduc.pr@gmail.com@46c6b5e2-aea9-36ba-7a94-b5413ff3bfb8>
2012-08-26 09:53:39 +0000

    - make ndk build

--- trunk/trunk/prjAndroid/gsgameplay/jni/updateSource.bat
+++ trunk/trunk/prjAndroid/gsgameplay/jni/updateSource.bat
@@ -1,6 +1,7 @@
 rem @echo off
-set VC_TOOL=..\..\..\..\Tools\VCPrjExport.exe
+set VC_TOOL=..\..\..\tools\vcProjExport\Release\VCPrjExport.exe
 
-%VC_TOOL% freetype_android.vcproj source_libfreetype.mak
-%VC_TOOL% libcurl_android.vcproj source_liburl.mak
-%VC_TOOL% toado_android.vcproj source_toado.mak
+%VC_TOOL% prj_lua.vcproj source_liblua.mak
+%VC_TOOL% prj_freetype.vcproj source_libfreetype.mak
+%VC_TOOL% prj_gameswf.vcproj source_gameswf.mak
+%VC_TOOL% prj_irrlicht.vcproj source_irlicht.mak

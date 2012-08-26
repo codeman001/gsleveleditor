56a1aaf3f79288fcb64e8a4cad04a368730b211d
hongduc.pr <hongduc.pr@gmail.com>
2012-08-26 09:53:39 +0000

    - make ndk build

--- trunk/prjAndroid/gsgameplay/jni/Android.mk
+++ trunk/prjAndroid/gsgameplay/jni/Android.mk
@@ -6,19 +6,3 @@ LOCAL_MODULE    := libfreetype
 include source_libfreetype.mak
 LOCAL_CFLAGS += -w -fsigned-char
 include $(BUILD_STATIC_LIBRARY)
-
-
-include $(CLEAR_VARS)
-LOCAL_MODULE    := libcurl
-include source_liburl.mak
-LOCAL_CFLAGS += -w -fsigned-char
-include $(BUILD_STATIC_LIBRARY)
-
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libapptoado
-include source_toado.mak
-LOCAL_CFLAGS += -w -fsigned-char
-LOCAL_STATIC_LIBRARIES := libfreetype libcurl
-LOCAL_LDLIBS := -lGLESv1_CM -ldl -llog -lc -lgcc
-include $(BUILD_SHARED_LIBRARY)
--- trunk/prjAndroid/gsgameplay/jni/build.bat
+++ trunk/prjAndroid/gsgameplay/jni/build.bat
@@ -1,8 +1,8 @@
 @echo on
 
-@set BASHPATH="D:\Android\cygwin\bin\bash.exe"
-@set PROJECTDIR="/cygdrive/d/MyProjects/C++/vietnam-mbmap/Project/Android/toado/jni"
-@set NDKDIR="/cygdrive/d/Android/android-ndk-r5b/ndk-build"
+@set BASHPATH="E:\DevTools\cygwin\bin\bash.exe"
+@set PROJECTDIR="/cygdrive/f/MyProjects/C++/gseditor/trunk/prjAndroid/gsgameplay/jni"
+@set NDKDIR="/cygdrive/e/DevTools/android-ndk-r7b/ndk-build"
 
 %BASHPATH% --login -c "cd %PROJECTDIR% && %NDKDIR%
 

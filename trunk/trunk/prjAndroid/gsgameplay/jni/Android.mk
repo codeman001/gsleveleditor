b83c494f3c80debf99a43ec21abd40744dad4d8f
hongduc.pr@gmail.com <hongduc.pr@gmail.com@46c6b5e2-aea9-36ba-7a94-b5413ff3bfb8>
2012-08-26 14:35:45 +0000

    - update make file for android

--- trunk/trunk/prjAndroid/gsgameplay/jni/Android.mk
+++ trunk/trunk/prjAndroid/gsgameplay/jni/Android.mk
@@ -1,4 +1,3 @@
-
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -6,3 +5,27 @@ LOCAL_MODULE    := libfreetype
 include source_libfreetype.mak
 LOCAL_CFLAGS += -w -fsigned-char
 include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := liblua
+include source_liblua.mak
+LOCAL_CFLAGS += -w -fsigned-char
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := libgameswf
+include source_gameswf.mak
+LOCAL_CFLAGS += -w -fsigned-char
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := libirrlicht
+include source_irlicht.mak
+LOCAL_CFLAGS += -w -fsigned-char
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := libgsgameplay
+include source_gameplay.mak
+LOCAL_CFLAGS += -w -fsigned-char
+include $(BUILD_SHARED_LIBRARY)
--- trunk/trunk/prjAndroid/gsgameplay/jni/updateSource.bat
+++ trunk/trunk/prjAndroid/gsgameplay/jni/updateSource.bat
@@ -5,3 +5,4 @@ set VC_TOOL=..\..\..\tools\vcProjExport\Release\VCPrjExport.exe
 %VC_TOOL% prj_freetype.vcproj source_libfreetype.mak
 %VC_TOOL% prj_gameswf.vcproj source_gameswf.mak
 %VC_TOOL% prj_irrlicht.vcproj source_irlicht.mak
+%VC_TOOL% prj_gsgameplay.vcproj source_gameplay.mak

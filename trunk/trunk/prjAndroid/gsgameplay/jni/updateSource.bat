8af3c6e72685145d8f499376a447e63ca4ec5df0
hongduc.pr@gmail.com <hongduc.pr@gmail.com@46c6b5e2-aea9-36ba-7a94-b5413ff3bfb8>
2012-08-28 17:40:33 +0000

    - update project android

--- trunk/trunk/prjAndroid/gsgameplay/jni/updateSource.bat
+++ trunk/trunk/prjAndroid/gsgameplay/jni/updateSource.bat
@@ -1,8 +1,9 @@
 rem @echo off
 set VC_TOOL=..\..\..\tools\vcProjExport\Release\VCPrjExport.exe
 
-%VC_TOOL% prj_lua.vcproj source_liblua.mak
-%VC_TOOL% prj_freetype.vcproj source_libfreetype.mak
-%VC_TOOL% prj_gameswf.vcproj source_gameswf.mak
+rem %VC_TOOL% prj_lua.vcproj source_liblua.mak
+rem %VC_TOOL% prj_freetype.vcproj source_libfreetype.mak
+rem %VC_TOOL% prj_gameswf.vcproj source_gameswf.mak
 %VC_TOOL% prj_irrlicht.vcproj source_irlicht.mak
-%VC_TOOL% prj_gsgameplay.vcproj source_gameplay.mak
+%VC_TOOL% prj_irrlichtEx.vcproj source_irlichtex.mak
+rem %VC_TOOL% prj_gsgameplay.vcproj source_gameplay.mak

bb7968f952e110e6d0ffcf734624bc0398e82450
hongduc.pr <hongduc.pr@gmail.com>
2012-09-01 10:04:26 +0000

    - add gles1 render on android

---
+++ trunk/prjAndroid/gsgameplay/src/com/gseditor/gsgameplay/GameGLES1Render.java
@@ -0,0 +1,21 @@
+package com.gseditor.gsgameplay;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLSurfaceView;
+
+public class GameGLES1Render implements GLSurfaceView.Renderer
+{
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) 
+	{	        
+	}
+
+	public void onSurfaceChanged(GL10 gl, int w, int h)
+	{	   
+	}
+
+	public void onDrawFrame(GL10 gl) 
+	{	
+	}
+}

83baa132a79f45a5e9a5ff3f3643f6aecd288b14
hongduc.pr@gmail.com <hongduc.pr@gmail.com@46c6b5e2-aea9-36ba-7a94-b5413ff3bfb8>
2012-09-03 05:51:28 +0000

    - add iPhone pro

---
+++ trunk/trunk/prjIphone/gameplay/gameplay/CAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CAppDelegate.h
+//  gameplay
+//
+//  Created by Pham Hong Duc on 9/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

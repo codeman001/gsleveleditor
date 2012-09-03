e77c4d938fb6d470728ab3d9ae797de4b916b3ff
hongduc.pr <hongduc.pr@gmail.com>
2012-09-03 05:51:28 +0000

    - add iPhone pro

---
+++ trunk/prjIphone/gameplay/gameplay/CViewController.h
@@ -0,0 +1,14 @@
+//
+//  CViewController.h
+//  gameplay
+//
+//  Created by Pham Hong Duc on 9/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface CViewController : GLKViewController
+
+@end

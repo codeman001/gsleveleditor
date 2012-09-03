b9249ee6d1af47a12a196cc22b8df428d820e97d
hongduc.pr <hongduc.pr@gmail.com>
2012-09-03 05:11:54 +0000

    - add iPhone project

---
+++ trunk/prjIphone/gsgameplay/gsgameplayTests/gsgameplayTests.h
@@ -0,0 +1,13 @@
+//
+//  gsgameplayTests.h
+//  gsgameplayTests
+//
+//  Created by Pham Hong Duc on 9/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface gsgameplayTests : SenTestCase
+
+@end

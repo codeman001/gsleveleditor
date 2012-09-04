1020d2037529a8e4645f03c08351f330fc840aed
hongduc.pr@gmail.com <hongduc.pr@gmail.com@46c6b5e2-aea9-36ba-7a94-b5413ff3bfb8>
2012-09-04 03:46:02 +0000

    - add packdata.bat

---
+++ trunk/trunk/bin/packdata.bat
@@ -0,0 +1,17 @@
+delete _common.zip
+delete _texture.zip
+delete _scene.zip
+
+..\tools\7z\7z.exe a -r _common.zip *.css
+..\tools\7z\7z.exe a -r _common.zip *.fsh
+..\tools\7z\7z.exe a -r _common.zip *.vsh
+..\tools\7z\7z.exe a -r _common.zip *.swf
+..\tools\7z\7z.exe a -r _common.zip *.lv
+..\tools\7z\7z.exe a -r _common.zip *.lua
+
+..\tools\7z\7z.exe a -r _texture.zip *.tga
+..\tools\7z\7z.exe a -r _texture.zip *.png
+
+..\tools\7z\7z.exe a -r _scene.zip *.scene
+..\tools\7z\7z.exe a -r _scene.zip *.anim
+

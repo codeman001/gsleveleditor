60149102942db7715a40a6694c4b3c746d5fbe79
hongduc.pr@gmail.com <hongduc.pr@gmail.com@46c6b5e2-aea9-36ba-7a94-b5413ff3bfb8>
2012-09-04 04:13:13 +0000

    - update zip package

--- trunk/trunk/bin/packdata.bat
+++ trunk/trunk/bin/packdata.bat
@@ -8,9 +8,9 @@ delete _scene.zip
 ..\tools\7z\7z.exe a -r _common.zip *.swf
 ..\tools\7z\7z.exe a -r _common.zip *.lv
 ..\tools\7z\7z.exe a -r _common.zip *.lua
+..\tools\7z\7z.exe a -r _common.zip *.png
 
 ..\tools\7z\7z.exe a -r _texture.zip *.tga
-..\tools\7z\7z.exe a -r _texture.zip *.png
 
 ..\tools\7z\7z.exe a -r _scene.zip *.scene
 ..\tools\7z\7z.exe a -r _scene.zip *.anim

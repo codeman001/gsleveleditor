cd9c0cc9aa287465bf4d5a8708d5a31d6733744c
hongduc.pr <hongduc.pr@gmail.com>
2012-09-06 03:00:49 +0000

    - update make for android

--- trunk/prjAndroid/gsgameplay/jni/Android.mk
+++ trunk/prjAndroid/gsgameplay/jni/Android.mk
@@ -3,19 +3,16 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE    := liblua
 include source_liblua.mak
-LOCAL_CFLAGS += -w -fsigned-char
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := libgameswf
 include source_gameswf.mak
-LOCAL_CFLAGS += -w -fsigned-char
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := libirrlicht
 include source_irlicht.mak
-LOCAL_CFLAGS += -w -fsigned-char
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -27,7 +24,7 @@ include $(BUILD_STATIC_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_MODULE    := libgsgameplay
 include source_gameplay.mak
-LOCAL_CFLAGS += -w -fsigned-char
+#LOCAL_CFLAGS += -w -fsigned-char
 LOCAL_STATIC_LIBRARIES := liblua libgameswf libirrlicht libirrlichtex
 #LOCAL_LDLIBS := -lGLESv2 -ldl -llog -lc -lgcc
 LOCAL_LDLIBS := -lGLESv1_CM -ldl -llog -lc -lgcc
--- trunk/prjAndroid/gsgameplay/jni/Application.mk
+++ trunk/prjAndroid/gsgameplay/jni/Application.mk
@@ -1,3 +1,4 @@
 APP_PLATFORM := android-8
-APP_CPPFLAGS += -frtti
+APP_CPPFLAGS += -frtti -w -fsigned-char -O3
 APP_STL := stlport_static
+APP_OPTIM := release

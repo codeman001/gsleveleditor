912c1a4020a2ea026351c95c9aad8bf25f2e40d4
hongduc.pr <hongduc.pr@gmail.com>
2012-09-18 08:00:07 +0000

    - update for build with directX9

--- extern/irrogles/include/IrrCompileConfig.h
+++ extern/irrogles/include/IrrCompileConfig.h
@@ -66,9 +66,11 @@
 #define _IRR_WINDOWS_API_
 #define _IRR_COMPILE_WITH_WINDOWS_DEVICE_
 //#define _IRR_COMPILE_WITH_OGLES1_
-#define _IRR_COMPILE_WITH_OGLES2_
-//#define _IRR_COMPILE_WITH_OPENGL_
+//#define _IRR_COMPILE_WITH_OGLES2_
+#define _IRR_COMPILE_WITH_OPENGL_
 
+// Build with windowsdk6 if use direct3d9
+//#define _IRR_COMPILE_WITH_DIRECT3D_9_
 #endif
 
 //! WINCE is a very restricted environment for mobile devices

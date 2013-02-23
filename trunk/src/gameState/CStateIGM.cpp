a0bae1a530fd7bb5b5492eba329520597c4e5a84
hongduc.pr <hongduc.pr@gmail.com>
2013-02-23 13:24:30 +0000

    - add igm

---
+++ trunk/src/gameState/CStateIGM.cpp
@@ -0,0 +1,111 @@
+//
+//  CStateIGM.cpp
+//  gameplay
+//
+//  Created by Pham Hong Duc on 2/23/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#include "stdafx.h"
+#include "CStateIGM.h"
+#include "CGameStateManager.h"
+#include "gameEvent.h"
+
+const int k_btnResumeGame = 0;
+const int k_btnMainMenu = 1;
+
+CStateIGM::CStateIGM()
+    :CGameState(GSStateIGM)
+{
+    m_menuChoice = -1;
+}
+
+CStateIGM::~CStateIGM()
+{
+    
+}
+
+void CStateIGM::onCreate()
+{
+    // show ui
+	m_menuFx = CGameUI::getInstance()->getFlash("uiGameHud");
+	
+	// show mainmenu state
+	showFxThisState();
+    
+    m_resumeButton = new CUIButton("btnResume", m_rootWidget, getStateObjFx().findObj("btnResume"));
+    m_resumeButton->setText("txtLabel", "RESUME GAME");
+
+    m_mainMenuButton = new CUIButton("btnMainMenu", m_rootWidget, getStateObjFx().findObj("btnMainMenu"));
+    m_mainMenuButton->setText("txtLabel", "MAIN MENU");    
+}
+
+void CStateIGM::onDestroy()
+{
+    
+}
+
+void CStateIGM::onFsCommand( const char *command, const char *param )
+{
+    if ( strcmp("stateStatus", command) == 0 )
+	{	
+		if ( strcmp("show",param) == 0 && m_needInitState == true )
+		{          
+			m_needInitState = false;			
+			m_rootWidget->lockActionOnChilld(true);
+		}
+		else if ( strcmp("active",param) == 0 )
+		{
+			// todo
+			// finish play show anim
+			m_rootWidget->lockActionOnChilld(false);
+		}
+		else if ( strcmp("close",param) == 0 )
+		{
+            if ( m_menuChoice == k_btnResumeGame )            
+                CGameStateMgr::getInstance()->popState();
+            else if ( m_menuChoice == k_btnMainMenu )
+            {
+                // todo quit game and joint to mainmenu
+            }
+        }
+	}
+}
+
+void CStateIGM::onRender()
+{
+    // render parent
+    CGameStateMgr::getInstance()->getStateBefore(this)->onRender();
+}
+
+void CStateIGM::onUpdate()
+{
+    CGameState::onUpdate();
+}
+
+void CStateIGM::onEvent(const SEvent& event)
+{
+    if ( event.EventType == EET_GAME_EVENT )
+    {
+		if ( event.GameEvent.EventID == (s32)EvtButtonRelease )
+        {
+            SEventButtonData *buttonEvent = ((SEventButtonData*)event.GameEvent.EventData);
+            
+            if ( buttonEvent->data == m_resumeButton )
+            {
+                m_menuChoice = k_btnResumeGame;
+            }
+            else if ( buttonEvent->data == m_mainMenuButton )
+            {
+                m_menuChoice = k_btnMainMenu;
+            }
+            
+            m_rootWidget->removeChild(m_resumeButton);
+            m_rootWidget->removeChild(m_mainMenuButton);
+            
+            // animation state
+            getStateObjFx().gotoFrame("hide", true);
+        }        
+    }
+}
+

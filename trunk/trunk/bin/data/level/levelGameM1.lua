d582e8263f08d2417c10e90a8dc261b5713fd3a9
hongduc.pr@gmail.com <hongduc.pr@gmail.com@46c6b5e2-aea9-36ba-7a94-b5413ff3bfb8>
2013-04-04 14:47:24 +0000

    - update level (+ remove character, + add spawnpoint)

--- trunk/trunk/bin/data/level/levelGameM1.lua
+++ trunk/trunk/bin/data/level/levelGameM1.lua
@@ -1,17 +1,7 @@
 debug("-----------------------------------------------------------------")
 debug("compile levelGameM1.lua")
 
-function triggerInitLevel_enable(triggerID)
-	-- setting player
-	local playerID = getObjectByName("player")
-	clearObjectLod( playerID )	
-		
-	-- setting camera
-	local cameraID = getObjectByName("playerCamera")
-	setLevelCamera(cameraID)
-	setCameraFarValue(cameraID, 10000)
-	setCameraFollowObject(cameraID, playerID, 350)
-	
+function triggerInitLevel_enable(triggerID)	
 	-- default ambient light
 	setLevelAmbientLight(100,100,100)	
 end

d8a7fac3808bfb95f34911a44ae9776cad24d70d
hongduc.pr@gmail.com <hongduc.pr@gmail.com@46c6b5e2-aea9-36ba-7a94-b5413ff3bfb8>
2013-04-20 13:03:43 +0000

    - update prj

--- trunk/trunk/src/gameState/CStateInit.cpp
+++ trunk/trunk/src/gameState/CStateInit.cpp
@@ -8,7 +8,7 @@
 #include "IView.h"
 #include "gameConfig/CGameConfig.h"
 
-//#define OPEN_GAMEPLAY	"data/level/levelGameM1.lv"
+#define OPEN_GAMEPLAY	"data/level/levelGameM1.lv"
 
 CStateInit::CStateInit()
 	:CGameState( CGameState::GSStateInit )

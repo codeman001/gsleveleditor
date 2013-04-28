d1aa2ff60446c3033fc627f135c6cf7c4e1f125c
hongduc.pr <hongduc.pr@gmail.com>
2013-04-28 03:33:29 +0000

    - update data

--- trunk/bin/packdata.bat
+++ trunk/bin/packdata.bat
@@ -10,6 +10,7 @@ delete _scene.zip
 ..\tools\7z\7z.exe a -r _common.zip *.lv
 ..\tools\7z\7z.exe a -r _common.zip *.lua
 ..\tools\7z\7z.exe a -r _common.zip *.png
+..\tools\7z\7z.exe a -r _common.zip *.xml
 
 ..\tools\7z\7z.exe a -r _textureTGA.zip *.tga
 ..\tools\7z\7z.exe a -r _texturePVR.zip *.pvr
